7dc2e3f2ea0c4a55be6c50d0631fd696998223ab
Steven Carpenter <stevegcarpenter@gmail.com>
2018-01-23 17:30:38 -0800

    Implemented the '/' root path for the server

--- lab-steve/__test__/server.test.js
+++ lab-steve/__test__/server.test.js
@@ -5,7 +5,7 @@ const superagent = require('superagent');
 
 describe('Server module', () => {
   beforeAll(() => server.start(5555));
-  afterAl(() => server.stop());
+  afterAll(() => server.stop());
 
   describe('Valid Request to the API', () => {
     describe('GET /', () => {
@@ -15,26 +15,40 @@ describe('Server module', () => {
             expect(res.status).toBe(200);
           });
       });
+
+      it('should respond with the message "Hello from my server!"', () => {
+        return superagent.get(':5555/')
+          .then(res => {
+            expect(res.text).toBe('Hello from my server!');
+          });
+      });
+
+      it('should respond with a header containing Content-Type: text/plain', () => {
+        return superagent.get(':5555/')
+          .then(res => {
+            expect(res.header['content-type']).toBe('text/plain');
+          });
+      });
     });
 
     // change to be GET /cowsay
-    describe('GET /', () => {
-      it('should respond with a status 200', () => {
-        return superagent.get(':5555/')
-          .then(res => {
-            expect(res.status).toBe(200);
-          });
-      });
-    });
+    // describe('GET /', () => {
+    //   it('should respond with a status 200', () => {
+    //     return superagent.get(':5555/')
+    //       .then(res => {
+    //         expect(res.status).toBe(200);
+    //       });
+    //   });
+    // });
 
-    // change to be POST /cowsay
-    describe('GET /', () => {
-      it('should respond with a status 200', () => {
-        return superagent.get(':5555/')
-          .then(res => {
-            expect(res.status).toBe(200);
-          });
-      });
-    });
+    // // change to be POST /cowsay
+    // describe('GET /', () => {
+    //   it('should respond with a status 200', () => {
+    //     return superagent.get(':5555/')
+    //       .then(res => {
+    //         expect(res.status).toBe(200);
+    //       });
+    //   });
+    // });
   });
 });
