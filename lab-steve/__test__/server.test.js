5ba077a5b13037e92be0a7569c6f2e2bec1fc1c9
Steven Carpenter <stevegcarpenter@gmail.com>
2018-01-23 22:59:48 -0800

    Added POST /cowsay route tests and reformatted tests

--- lab-steve/__test__/server.test.js
+++ lab-steve/__test__/server.test.js
@@ -11,44 +11,34 @@ describe('Server module', () => {
     describe('GET /', () => {
       it('should respond with a status 200', () => {
         return superagent.get(':5555/')
-          .then(res => {
-            expect(res.status).toBe(200);
-          });
+          .then(res => expect(res.status).toBe(200));
       });
 
       it('should respond with the message "Hello from my server!"', () => {
         return superagent.get(':5555/')
-          .then(res => {
-            expect(res.text).toBe('Hello from my server!');
-          });
+          .then(res => expect(res.text).toBe('Hello from my server!'));
       });
 
       it('should respond with a header containing Content-Type: text/plain', () => {
         return superagent.get(':5555/')
-          .then(res => {
-            expect(res.header['content-type']).toBe('text/plain');
-          });
+          .then(res => expect(res.header['content-type']).toBe('text/plain'));
       });
     });
 
     describe('GET /cowsay', () => {
       it('should respond with a status 200', () => {
         return superagent.get(':5555/')
-          .then(res => {
-            expect(res.status).toBe(200);
-          });
+          .then(res => expect(res.status).toBe(200));
       });
 
-      it('should result in a cowsay Bad Request', () => {
+      it('should result in a cowsay Bad Request if no query exists', () => {
         return superagent.get(':5555/cowsay')
-          .catch(err => expect(err.response.text).toMatch(/Bad Request/))
+          .catch(err => expect(err.response.text).toMatch(/Bad Request/));
       });
 
       it('should respond with a header containing Content-Type: text/plain', () => {
-        return superagent.get(':5555/')
-          .then(res => {
-            expect(res.header['content-type']).toBe('text/plain');
-          });
+        return superagent.get(':5555/cowsay?text=foo')
+          .then(res => expect(res.header['content-type']).toBe('text/plain'));
       });
 
       it('should display the message the user requests', () => {
@@ -57,14 +47,23 @@ describe('Server module', () => {
       });
     });
 
-    // change to be POST /cowsay
-    // describe('GET /', () => {
-    //   it('should respond with a status 200', () => {
-    //     return superagent.get(':5555/')
-    //       .then(res => {
-    //         expect(res.status).toBe(200);
-    //       });
-    //   });
-    // });
+    describe('POST /cowsay', () => {
+      it('should respond with a status 200 for success', () => {
+        return superagent.post(':5555/cowsay')
+          .send({text: 'Amazing!'})
+          .then(res => expect(res.status).toBe(200));
+      });
+
+      it('should respond with the appropriate cowsay message', () => {
+        return superagent.post(':5555/cowsay')
+          .send({text: 'Amazing!'})
+          .then(res => expect(res.text).toMatch(/Amazing!/));
+      });
+
+      it('should respond in a cowsay Bad Request when no data exists', () => {
+        return superagent.post(':5555/cowsay')
+          .catch(err => expect(err.response.text).toMatch(/Bad Request/));
+      });
+    });
   });
 });
