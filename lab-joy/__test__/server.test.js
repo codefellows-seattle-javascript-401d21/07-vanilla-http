ef03601799d4aa7b22adc00ea6d9207f68f89a99
strawbee <yuejiao.hou@gmail.com>
2018-01-24 07:37:10 -0800

    tests done

--- lab-joy/__test__/server.test.js
+++ lab-joy/__test__/server.test.js
@@ -11,13 +11,13 @@ describe('Server Module', () => {
         it('should respond with a status 200', () => {
             return superagent.get(':3000/')
                 .then(res => expect(res.status).toBe(200))
-                .catch(err => console.error(err));
+                .catch();
         });
 
         it('should respond with "Hello from my server!"', () => {
             return superagent.get(':3000/')
                 .then(res => expect(res.text).toBe('Hello from my server!'))
-                .catch(err => console.error(err));
+                .catch();
         });
     });
 
@@ -25,34 +25,71 @@ describe('Server Module', () => {
         it('should respond with a status 200', () => {
             return superagent.get(':3000/cowsay?text="hello"')
                 .then(res => expect(res.status).toBe(200))
-                .catch(err => expect(err.status).toBe(400));
+                .catch();
         });
 
         it('should respond with the querystring', () => {
             return superagent.get(':3000/cowsay?text="hello"')
                 .then(res => expect(res.text).toMatch(/hello/))
-                .catch(err => expect(err.status).toBe(400));
+                .catch();
         });
     });
 
     describe('Invalid GET Request to the /cowsay', () => {
         it('should respond with a status 200', () => {
-            return superagent.get(':3000/cowsay?')
-                .then(res => expect(res.status).toBe(200))
+            return superagent.get(':3000/cowsay')
+                .then()
                 .catch(err => expect(err.status).toBe(400));
         });
 
         it('should respond with "bad request"', () => {
             return superagent.get(':3000/cowsay')
-                .then(res => expect(res.text).toMatch(/bad request/));
-                .catch(err => expect(err.text).toMatch(/bad request/));
+                .then()
+                .catch(err => expect(err.response.text).toMatch(/bad request/));
         });
-
-        // it('should respond with the querystring', () => {
-        //     return superagent.get(':3000/cowsay?text="hello"')
-        //         .then(res => expect(res.text).toMatch(/hello/))
-        //         .catch(err => console.error(err));
-        // });
     });
 
+    describe('Valid POST request to /cowsay', () => {
+        it('should respond with a status 200', () => {
+            return superagent.post(':3000/cowsay')
+                .send({text: 'hello'})
+                .then(res => expect(res.status).toBe(200))
+                .catch();
+        });
+
+        it('should respond with the body text', () => {
+            return superagent.post(':3000/cowsay')
+                .send({ text: 'hello' })
+                .then(res => expect(res.text).toMatch(/hello/))
+                .catch();
+        });
+    });
+
+    describe('Invalid POST request to /cowsay', () => {
+        it('should respond with a status 400', () => {
+            return superagent.post(':3000/cowsay')
+                .then()
+                .catch(err => expect(err.status).toBe(400));
+        });
+
+        it('should respond with "bad request"', () => {
+            return superagent.post(':3000/cowsay')
+                .then()
+                .catch(err => expect(err.response.text).toMatch(/bad request/));
+        });
+    });
+
+    describe('Invalid filepath', () => {
+        it('should respond with a status 404', () => {
+            return superagent.get(':3000/cats')
+                .then()
+                .catch(err => expect(err.status).toBe(404));
+        });
+
+        it('should respond with "Not Found"', () => {
+            return superagent.get(':3000/cats')
+                .then()
+                .catch(err => expect(err.response.text).toBe('Not Found'));
+        });
+    });
 });
