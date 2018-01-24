dc7d513d7a81babce713c1a596119401476d650c
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-01-23 18:28:38 -0800

    add server

--- lab-ed/index.js
+++ lab-ed/index.js
@@ -1,4 +1,4 @@
 'use strict'
 
-const server = require('../lib/server')
-const superagen = require('superagent')
+const server = require('./lib/server')
+server.start(3000, () => console.log(`Listening on port 3000`))
--- lab-ed/lib/server.js
+++ lab-ed/lib/server.js
@@ -2,3 +2,39 @@
 
 const http = require('http')
 const bodyParser = require('./body-parse')
+
+const app = http.createServer((req, res) => {
+  bodyParser(req)
+    .then(request => {
+      if(request.method === 'GET' && request.url.pathname === '/time') {
+        res.writeHead(200, {'Content-Type': 'application/json'})
+        res.write(JSON.stringify({
+          now: Date.now(),
+          date: new Date(),
+        }))
+        res.end()
+        return
+      }
+
+      if(request.method === 'POST' && request.url.pathname === 'echo') {
+        res.writeHead(200, {'Content-Type': 'application/json'})
+        res.write(JSON.stringify(request.body))
+        res.end()
+        return
+      }
+
+      res.writeHead(404, {'Content-Type': 'text/plain'})
+      res.write('Not Found')
+      res.end()
+      return
+    })
+    .catch(error => {
+      res.writeHead(400, { 'Content-Type': 'text/plain' })
+      res.write('Bad Request')
+      res.end()
+    })
+})
+
+const server = module.exports = {}
+server.start = (port, callback) => app.listen(port, callback)
+server.stop = (callback) => app.close(callback)
