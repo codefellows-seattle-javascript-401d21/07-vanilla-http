94413e9ee38353a63142126bbc404934347b6dcb
Steven Carpenter <stevegcarpenter@gmail.com>
2018-01-23 22:32:21 -0800

    Added failure test for GET /cowsay

--- lab-steve/__test__/server.test.js
+++ lab-steve/__test__/server.test.js
@@ -39,20 +39,25 @@ describe('Server module', () => {
           });
       });
 
-      it('should ask for something to say if no text is given', () => {
+      it('should result in a cowsay Bad Request', () => {
         return superagent.get(':5555/cowsay')
+          .catch(err => expect(err.response.text).toMatch(/Bad Request/))
+      });
+
+      it('should respond with a header containing Content-Type: text/plain', () => {
+        return superagent.get(':5555/')
           .then(res => {
-            expect(res.text).toMatch(/You need to tell me what to say!/);
+            expect(res.header['content-type']).toBe('text/plain');
           });
       });
 
       it('should display the message the user requests', () => {
         return superagent.get(':5555/cowsay?text=Yikes!')
           .then(res => expect(res.text).toMatch(/Yikes!/));
-      })
+      });
     });
 
-    // // change to be POST /cowsay
+    // change to be POST /cowsay
     // describe('GET /', () => {
     //   it('should respond with a status 200', () => {
     //     return superagent.get(':5555/')
