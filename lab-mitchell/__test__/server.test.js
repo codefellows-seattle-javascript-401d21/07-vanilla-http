e1039bc16c9053de31a4247df645273d1ecf30e1
Mitchell M <mitchellm1618@gmail.com>
2018-01-23 21:51:04 -0800

    comitting work, tests not yet running

--- lab-mitchell/__test__/server.test.js
+++ lab-mitchell/__test__/server.test.js
@@ -2,4 +2,22 @@
 
 const server = require('../lib/server');
 const superagent = require('superagent');
+const cowsay = require('cowsay');
 
+describe('server.js', function() {
+  beforeAll(() => server.start(3339));
+  afterAll(() => server.stop());
+
+  describe('Valid request', function() {
+    it('should respond status 200 for valid request', function(done) {
+      superagent.get(':3339/cowsay').then((res, req) => {
+        expect(cowsay.say({text: 'hello'}).toBe('hello'));
+        expect(res.status).toBe(200);
+        done();
+      });
+    });
+
+  });
+
+
+});
