a6bcf056057aa5b4f2c2081f250ebe8491ebe304
Melanie Downing <sayanything830@gmail.com>
2018-01-23 18:21:52 -0800

    get and post functions done, on to testing

--- lab-melanie/lib/server.js
+++ lab-melanie/lib/server.js
@@ -15,15 +15,26 @@ const app = http.createServer((request, response) => {
         response.end();
         return;
       }
-      if(req.method === 'GET' && req.url.pathname === '/cowsay' && req.url.query) {
+      if(req.method === 'GET' && req.url.pathname === '/cowsay') {
         console.log(req.url.query);
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.write(cowsay.say({
-          text: req.url.query,
-        }));
+        if(req.url.query) {
+          response.writeHead(200, {'Content-Type': 'text/plain'});
+          response.write(cowsay.say({
+            text: req.url.query,
+          }));
+          response.end();
+          return;
+        }
+      }
+
+
+      if(req.method === 'POST' && req.url.pathname === '/cowsay') {
+        response.writeHead(201, {'Content-Type': 'application/json'});
+        response.write(JSON.stringify(req.body));
         response.end();
         return;
       }
+
       response.writeHead(404, {'Content-Type': 'text/plain'});
       response.write(cowsay.say({
         text: 'Not Found',
@@ -36,7 +47,7 @@ const app = http.createServer((request, response) => {
       response.write(cowsay.say({
         text: 'Bad Request',
       }));
-      repsonse.end();
+      response.end();
       return;
     });
 });
